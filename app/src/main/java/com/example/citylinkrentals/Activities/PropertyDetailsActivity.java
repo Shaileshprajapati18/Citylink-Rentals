338534b560d833e05e4c70c356d90173034b926d
Shaileshprajapati18 <shaileshprajapati182005@gmail.com>
2025-08-21 19:47:21 +0530

    new push

--- app/src/main/java/com/example/citylinkrentals/Activities/PropertyDetailsActivity.java
+++ app/src/main/java/com/example/citylinkrentals/Activities/PropertyDetailsActivity.java
@@ -1,9 +1,11 @@
 package com.example.citylinkrentals.Activities;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,5 +20,11 @@ public class PropertyDetailsActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_property_details);
 
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.white));
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
